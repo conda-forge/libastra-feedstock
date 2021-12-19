ba99c65e1c33ca1f37ef9423edeab8b4d92e8ff1
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-19 16:38:58 -0600

    Add more debug information to windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,10 @@
+echo "C compiler is %CC%"
+%CC%
+echo "CXX compiler is %CXX%"
+%CXX%
+echo "CUDA path is %CUDA_PATH%"
 for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
-echo "nvcc is %CUDACXX%, CUDA path is %CUDA_PATH%"
-echo "CMAKE_ARGS are %CMAKE_ARGS%"
+echo "CUDA compiler is %CUDACXX%"
+echo "CUDA host compiler is %CUDAHOSTCXX%"
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %CMAKE_ARGS% %SRC_DIR%
 ninja install
