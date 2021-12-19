e4f58d67c5a4f04ec7c5749fa54061f3d6b814c3
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-19 17:34:15 -0600

    Set CUDA host and device compiler manually

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,9 @@
+:: Manually specify CUDA host compiler instead of relying on setup scripts
+:: https://gitter.im/conda-forge/conda-forge.github.io?at=5f8901b86c8d484be2898fc5
+
+set CUDACXX="%CUDA_PATH%\bin\nvcc.exe"
+set CUDAHOSTCXX="%CXX%"
+
 echo "C compiler is %CC%"
 %CC%
 echo "CXX compiler is %CXX%"
@@ -6,5 +12,7 @@ echo "CUDA path is %CUDA_PATH%"
 for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 echo "CUDA compiler is %CUDACXX%"
 echo "CUDA host compiler is %CUDAHOSTCXX%"
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %CMAKE_ARGS% %SRC_DIR%
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%^
+  %CMAKE_ARGS% %SRC_DIR%
 ninja install
