9838ccc1ff1560bd99f72abc9e6b7ea9da09cfd1
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-19 19:33:09 -0600

    Try new compiler setting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,18 @@
 :: Manually specify CUDA host compiler instead of relying on setup scripts
 :: https://gitter.im/conda-forge/conda-forge.github.io?at=5f8901b86c8d484be2898fc5
 
-set CUDACXX="%CUDA_PATH%\bin\nvcc.exe"
-set CUDAHOSTCXX="%CXX%"
+set "CUDACXX=%CUDA_HOME%\bin\nvcc.exe"
+set "CUDAHOSTCXX=%CXX%"
 
 echo "C compiler is %CC%"
 %CC%
 echo "CXX compiler is %CXX%"
 %CXX%
 echo "CUDA path is %CUDA_PATH%"
+echo "CUDA home is %CUDA_HOME%"
 echo "CUDA compiler is %CUDACXX%"
 echo "CUDA host compiler is %CUDAHOSTCXX%"
+echo "CMAKE_ARGS is %CMAKE_ARGS%"
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%^
   %CMAKE_ARGS% %SRC_DIR%
