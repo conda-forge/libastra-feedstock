caacfd979dccd2d9f3b1cca93cce4e83b3cc7d24
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-19 18:35:11 -0600

    BUG: Don't reset CUDACXX after setting manually

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,6 @@ echo "C compiler is %CC%"
 echo "CXX compiler is %CXX%"
 %CXX%
 echo "CUDA path is %CUDA_PATH%"
-for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 echo "CUDA compiler is %CUDACXX%"
 echo "CUDA host compiler is %CUDAHOSTCXX%"
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release^
