b8ccdc03d241b44d1f01b8d978c4672f95d76964
Daniel Ching <carterbox@users.noreply.github.com>
2022-01-14 14:52:42 -0600

    Tweak windows build script more

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,15 +15,19 @@ echo "CUDA compiler is %CUDACXX%"
 "%CUDACXX%" --version
 echo "CUDA host compiler is %CUDAHOSTCXX%"
 echo "CMAKE_ARGS is %CMAKE_ARGS%"
+echo "SRC_DIR is %SRC_DIR%"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Overriding activation scripts does not work
 ::  -DCMAKE_CUDA_COMPILER="%CUDACXX:\=/%" ^
 ::  -DCMAKE_CUDA_HOST_COMPILER="%CUDAHOSTCXX:\=/%" ^
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-  %CMAKE_ARGS% ^
-  %SRC_DIR%
-if errorlevel 1 exit 1
+cmake -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      %CMAKE_ARGS% %SRC_DIR%
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-ninja install
-if errorlevel 1 exit 1
+ninja install %SRC_DIR%
+if %errorlevel% neq 0 exit /b %errorlevel%
+exit /b 0
