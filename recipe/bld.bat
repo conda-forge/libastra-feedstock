7e6129bbcc70314dbf1738ecfbaee6f592f7ee60
Daniel Ching <carterbox@users.noreply.github.com>
2022-06-01 17:14:19 -0500

    Let CMake autodetect CUDA

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
-for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 echo "nvcc is %CUDACXX%, CUDA path is %CUDA_PATH%"
 echo "CMAKE_ARGS are %CMAKE_ARGS%"
 
@@ -9,7 +8,6 @@ cmake -GNinja^
   -DCMAKE_BUILD_TYPE=Release^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"^
   -DCMAKE_CXX_COMPILER:FILEPATH="%CXX%"^
-  -DCMAKE_CUDA_COMPILER:FILEPATH="%CUDACXX%"^
   %CMAKE_ARGS%^
   %SRC_DIR%
 if errorlevel 1 exit /b 1
