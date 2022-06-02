1d4ce535766d7b05f78cd95a9c436793c60c14ec
Daniel Ching <carterbox@users.noreply.github.com>
2022-06-02 13:28:07 -0500

    Manually set CUDAARCHS on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,9 @@ set CUDACXX="%CUDA_PATH%\bin\nvcc.exe"
 set "CUDACXX=%CUDACXX:\=/%"
 set "CXX=%CXX:\=/%"
 
+@REM Conda won't detect CUDA compiler version so manually set archs
+set CUDAARCHS="35;50;60;70;80"
+
 echo "CUDACXX is %CUDACXX%"
 echo "CXX is %CXX%"
 echo "CUDA_PATH is %CUDA_PATH%"
