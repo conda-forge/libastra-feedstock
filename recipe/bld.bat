229e0d1304ecf110b9202f0b759aebce481937e7
Daniel Ching <carterbox@users.noreply.github.com>
2022-09-27 19:40:44 -0500

    MAI: Reenable windows CUDA builds

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,8 @@
-@REM This explicit path requires that the exact cudatoolkit is installed
-set CUDACXX="%CUDA_PATH%\bin\nvcc.exe"
-set "CUDACXX=%CUDACXX:\=/%"
-set "CXX=%CXX:\=/%"
-
-@REM Conda won't detect CUDA compiler version so manually set archs
-set CUDAARCHS="35;50;60;70;80"
-
-echo "CUDACXX is %CUDACXX%"
-echo "CXX is %CXX%"
-echo "CUDA_PATH is %CUDA_PATH%"
-echo "CMAKE_ARGS are %CMAKE_ARGS%"
-
 cd .\build
 if errorlevel 1 exit /b 1
 
 cmake -GNinja^
+  -DBUILD_SHARED_LIBS:BOOL=ON^
   -DCMAKE_BUILD_TYPE=Release^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"^
   -DASTRA_USE_CUDA:BOOL=%ASTRA_USE_CUDA%^
