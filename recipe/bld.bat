49d2d693098062cb0a5e04cf36796932d77b825e
Alexander Skorikov <alexander.skorikov@gmail.com>
2026-04-08 15:10:36 +0200

    Fix win build not using ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,16 +4,8 @@ mkdir build
 cd build
 if errorlevel 1 exit /b 1
 
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CXX=%%dummy:\=\\%%")
-echo %CXX%
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CC=%%dummy:\=\\%%")
-echo %CC%
-
-set "CMAKE_GENERATOR_PLATFORM="
-set "CMAKE_GENERATOR_TOOLSET="
-
-:: Conda-forge CUDA build environment does not work with Ninja
 cmake^
+  -GNinja^
   -DBUILD_SHARED_LIBS:BOOL=ON^
   -DCMAKE_BUILD_TYPE=Release^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"^
